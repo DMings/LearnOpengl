291b2c1edf21bb538eed72c674db663f30de2309
区达明 <756213932@qq.com>
2019-09-05 09:33:14 +0800

    拿oes绘制有毛病

--- app/src/main/java/com/dming/testopengl/test/TestActivity.java
+++ app/src/main/java/com/dming/testopengl/test/TestActivity.java
@@ -30,7 +30,7 @@ public class TestActivity extends AppCompatActivity {
     private LineGraph mLineGraph;
     private NoFilter mNoFilter;
 //    private int mFrameBufferTexture = -1;
-    private int mOESTexture = 1;
+    private int mOESTexture = -1;
     private Handler mHandler;
     private HandlerThread mHandlerThread;
 
@@ -50,9 +50,7 @@ public class TestActivity extends AppCompatActivity {
                 DLog.i("mOESTexture draw: " + mOESTexture);
                 if (mOESTexture != -1) {
                     mSurfaceTexture.updateTexImage();
-//                    GLES20.glClearColor(1,1,1,1);
                     GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-//                    mLineGraph.onDraw(0,0,0,200,200);
                     mNoFilter.onDraw(mOESTexture, 0, 0, 200, 200);
                     mEglHelper.swapBuffers();
                     FGLUtils.glCheckErr();
@@ -64,6 +62,7 @@ public class TestActivity extends AppCompatActivity {
             public void surfaceCreated(SurfaceHolder holder) {
                 DLog.i("EglHelper surfaceCreated: " + Thread.currentThread().getName());
                 mEglHelper.initEgl(null, holder.getSurface());
+                mOESTexture = FGLUtils.createOESTexture();
                 FGLUtils.glCheckErr();
                 mLineGraph = new LineGraph(TestActivity.this);
                 mNoFilter = new NoFilter(TestActivity.this);
@@ -95,30 +94,18 @@ public class TestActivity extends AppCompatActivity {
         mHandler.post(new Runnable() {
             @Override
             public void run() {
-                DLog.i("HandlerThread: " + Thread.currentThread().getName() + " >>> ");
                 mSurfaceTexture = new SurfaceTexture(mOESTexture);
                 mSurface = new Surface(mSurfaceTexture);
+                mSurfaceTexture.setDefaultBufferSize(200,200);
                 mEglHelper2.initEgl(mEglHelper.getEglContext(), mSurface);
                 FGLUtils.glCheckErr();
                 GLES20.glViewport(0, 0, 200, 200);
                 TestLineGraph mLineGraph = new TestLineGraph(TestActivity.this);
-//                NormalFilter mNoFilter = new NormalFilter(TestActivity.this);
-//                mNoFilter.onChange(200, 200, 0);
-//                int[] ids = FGLUtils.createFBO(200, 200);
-//                DLog.i("ids>>>" + ids);
-//                if (ids != null) {
-//                    int frameBuffer = ids[0];
-//                    mFrameBufferTexture = ids[1];
-//                    GLES20.glClearColor(1, 1, 1, 1);
-//                    GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, frameBuffer);
                 GLES20.glClearColor(0, 0, 1, 1);
-                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
                 mLineGraph.onDraw(0, 0, 0, 200, 200);
-//                    GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
                 FGLUtils.glCheckErr();
-//                    mNoFilter.onDraw(mFrameBufferTexture, 0, 0, 200, 200);
                 mEglHelper2.swapBuffers();
-//                }
             }
         });
     }
