ecba110e9af1cda2cbc6089694e77bbca9891c07
区达明 <756213932@qq.com>
2019-09-07 21:18:42 +0800

    0.6.0 FBO

--- app/src/main/java/com/dming/testopengl/test/TestActivity.java
+++ app/src/main/java/com/dming/testopengl/test/TestActivity.java
@@ -34,11 +34,11 @@ public class TestActivity extends AppCompatActivity {
     private EglHelper mEglHelper2 = new EglHelper();
     private LineGraph mLineGraph;
     private NormalFilter mNoFilter;
-    //    private int mFrameBufferTexture = -1;
-    private int mOESTexture = -1;
+    private int mFrameBufferTexture = -1;
+    //    private int mOESTexture = -1;
+//    private int mTexture = -1;
     private Handler mHandler;
     private HandlerThread mHandlerThread;
-    private int mTestTexture = -1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,7 +53,7 @@ public class TestActivity extends AppCompatActivity {
         findViewById(R.id.btn_test_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                DLog.i("mOESTexture draw: " + mOESTexture);
+                DLog.i("mFrameBufferTexture draw: " + mFrameBufferTexture);
 //                if (mOESTexture != -1) {
 //                    mSurfaceTexture.updateTexImage();
 //                    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
@@ -61,6 +61,15 @@ public class TestActivity extends AppCompatActivity {
 //                    mEglHelper.swapBuffers();
 //                    FGLUtils.glCheckErr();
 //                }
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+                int w = 200;
+                int h = 200;
+                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mFrameBufferTexture);
+                mNoFilter.onDraw(mFrameBufferTexture, 0, 0, w, h);
+                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+                FGLUtils.glCheckErr("mFrameBufferTexture");
+                mEglHelper.swapBuffers();
             }
         });
         mTestSv.getHolder().addCallback(new SurfaceHolder.Callback() {
@@ -68,8 +77,7 @@ public class TestActivity extends AppCompatActivity {
             public void surfaceCreated(SurfaceHolder holder) {
                 DLog.i("EglHelper surfaceCreated: " + Thread.currentThread().getName());
                 mEglHelper.initEgl(null, holder.getSurface());
-                mOESTexture = FGLUtils.createOESTexture();
-                mTestTexture = FGLUtils.createTexture();
+//                mOESTexture = FGLUtils.createOESTexture();
                 FGLUtils.glCheckErr();
                 mLineGraph = new LineGraph(TestActivity.this);
                 mNoFilter = new NormalFilter(TestActivity.this);
@@ -78,42 +86,20 @@ public class TestActivity extends AppCompatActivity {
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                DLog.i("EglHelper surfaceChanged: "+mTestTexture);
+                DLog.i("EglHelper surfaceChanged: ");
                 mLineGraph.onChange(width, height, 0);
                 mNoFilter.onChange(width, height, 0);
                 GLES20.glClearColor(1, 1, 1, 1);
+                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
                 mLineGraph.onDraw(0, 0, 0, width, height);
                 FGLUtils.glCheckErr(11);
-                int w = 200;
-                int h = 200;
-                int fmt = 4;
-                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(w * h * fmt);
-                byteBuffer.order(ByteOrder.nativeOrder());
-                byteBuffer.position(0);
-                for (int i = 0; i < w * h * fmt; i += fmt) {
-                    byteBuffer.put((byte) 0x00);
-                    byteBuffer.put((byte) 0x00);
-                    byteBuffer.put( (byte) 0xff);
-                    byteBuffer.put((byte) 0xFF);
-                }
-                byteBuffer.position(0);
-                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTestTexture);
-                FGLUtils.glCheckErr(22);
-                GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, w, h, 0,
-                        GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, byteBuffer);
-                FGLUtils.glCheckErr(33);
-                mNoFilter.onDraw(mTestTexture, 0, 0, w, h);
-                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
 
-                ByteBuffer readByte = ByteBuffer.allocateDirect(w * h * fmt);
-                GLES20.glReadPixels(0,0,w,h,GLES20.GL_RGBA,GLES20.GL_UNSIGNED_BYTE,readByte);
-                DLog.i("readByte 0: "+readByte.get(0) + " 1: " + readByte.get(1)+ " 2: " + readByte.get(2)+ " 3: " + readByte.get(3));
-                Bitmap bitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
-                bitmap.copyPixelsFromBuffer(readByte);
-
-                mTestIv.setImageBitmap(bitmap);
-                FGLUtils.glCheckErr(44);
+//                ByteBuffer readByte = ByteBuffer.allocateDirect(w * h * fmt);
+//                GLES20.glReadPixels(0,0,w,h,GLES20.GL_RGBA,GLES20.GL_UNSIGNED_BYTE,readByte);
+//                DLog.i("readByte 0: "+readByte.get(0) + " 1: " + readByte.get(1)+ " 2: " + readByte.get(2)+ " 3: " + readByte.get(3));
+//                Bitmap bitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
+//                bitmap.copyPixelsFromBuffer(readByte);
+//                mTestIv.setImageBitmap(bitmap);
                 mEglHelper.swapBuffers();
             }
 
@@ -130,18 +116,46 @@ public class TestActivity extends AppCompatActivity {
         mHandler.post(new Runnable() {
             @Override
             public void run() {
-                mSurfaceTexture = new SurfaceTexture(mOESTexture);
+                int w = 200;
+                int h = 200;
+                int fmt = 4;
+                mSurfaceTexture = new SurfaceTexture(0);
                 mSurface = new Surface(mSurfaceTexture);
-                mSurfaceTexture.setDefaultBufferSize(200, 200);
+                mSurfaceTexture.setDefaultBufferSize(w, h);
                 mEglHelper2.initEgl(mEglHelper.getEglContext(), mSurface);
-                FGLUtils.glCheckErr();
-                GLES20.glViewport(0, 0, 200, 200);
-                TestLineGraph mLineGraph = new TestLineGraph(TestActivity.this);
-                GLES20.glClearColor(0, 0, 1, 1);
-                GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-                mLineGraph.onDraw(0, 0, 0, 200, 200);
-                FGLUtils.glCheckErr();
-                mEglHelper2.swapBuffers();
+                LineGraph mLineGraph = new LineGraph(TestActivity.this);
+                mLineGraph.onChange(w, h, 0);
+                int[] ids = FGLUtils.createFBO(w, h);
+                if (ids != null) {
+                    int frameBuffer = ids[0];
+                    mFrameBufferTexture = ids[1];
+                    GLES20.glViewport(0, 0, w, h);
+                    GLES20.glClearColor(0, 0, 1, 1);
+                    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+                    FGLUtils.glCheckErr("ByteBuffer>");
+                    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(w * h * fmt);
+                    byteBuffer.order(ByteOrder.nativeOrder());
+                    for (int i = 0; i < w * h * fmt; i += fmt) {
+                        byteBuffer.put((byte) 0x00);
+                        byteBuffer.put((byte) 0x00);
+                        byteBuffer.put((byte) 0xff);
+                        byteBuffer.put((byte) 0xFF);
+                    }
+                    byteBuffer.position(0);
+                    GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+                    GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mFrameBufferTexture);
+                    FGLUtils.glCheckErr(22);
+                    GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, w, h, 0,
+                            GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, byteBuffer);
+
+                    GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, frameBuffer);
+                    mLineGraph.onDraw(mFrameBufferTexture, 0, 0, w, h);
+                    GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+
+                    GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+                    FGLUtils.glCheckErr("mEglHelper2");
+                    mEglHelper2.swapBuffers();
+                }
             }
         });
     }
